f51d916fd526061475505bf0a6a275716134b0a2
Rehman Akbar <rana496@gmail.com>
2024-12-30 00:11:10 +0500

    Add routes for articles and user preferences, including public and protected endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\API\ArticleController;
+use App\Http\Controllers\API\UserPreferencesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
@@ -27,4 +29,16 @@ Route::middleware(['auth:sanctum', 'ensure.valid.token'])->group(function () {
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+// Public routes
+Route::get('/articles', [ArticleController::class, 'index']);
+Route::get('/articles/{article}', [ArticleController::class, 'show']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/preferences', [UserPreferencesController::class, 'show']);
+    Route::put('/preferences', [UserPreferencesController::class, 'update']);
+    Route::get('/feed', [UserPreferencesController::class, 'personalizedFeed']);
 });
